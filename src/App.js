f64c7e344915b09b085c6185b8dba9d7d1596586
universal developer <69350489+universal-developer@users.noreply.github.com>
2022-10-28 00:30:11 +0200

    Made new section 🦁

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import About from './components/About'
 import AllInOne from './components/AllInOne'
+import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Pricing from './components/Pricing'
 import Support from './components/Support'
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
       <About />
       <Support />
       <AllInOne />
+      <Pricing />
+      <Footer />
     </>
   )
 }
